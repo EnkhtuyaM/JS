2b4277266b4edf0e22f9c437867a84f0db64bbe0
EnkhtuyaM <enkhtuyam1001@gmail.com>
2023-12-06 12:37:41 +0800

    commit3

---
+++ Bodlogo/classwork1.js
@@ -0,0 +1,99 @@
+let products = [
+  {
+    name: "ABC songs",
+    price: 15,
+    currency: "USD",
+    category: "book",
+    brand: "ABC",
+    stock: 10,
+  },
+
+  {
+    name: "rice coocker",
+    price: 150,
+    currency: "USD",
+    category: "electronic",
+    brand: "LG",
+    stock: 3,
+  },
+
+  {
+    name: "iphone14",
+    price: 1000,
+    currency: "USD",
+    category: "electronic",
+    brand: "Apple",
+    stock: 100,
+  },
+
+  {
+    name: "bed sheet",
+    price: 75,
+    currency: "USD",
+    category: "home",
+    brand: "Calvin Klein",
+    stock: 10,
+  },
+
+  {
+    name: "toy",
+    price: 15,
+    currency: "USD",
+    category: "kids",
+    brand: "ABC",
+    stock: 10,
+  },
+
+  {
+    name: "pajama",
+    price: 115,
+    currency: "USD",
+    category: "cloth",
+    brand: "Gap",
+    stock: 1000,
+  },
+  {
+    name: "curtain",
+    price: 15,
+    currency: "USD",
+    category: "home",
+    brand: "ABC",
+    stock: 103,
+  },
+
+  {
+    name: "iron",
+    price: 111,
+    currency: "USD",
+    category: "electronic",
+    brand: "Sony",
+    stock: 10,
+  },
+
+  {
+    name: "dish washer",
+    price: 15,
+    currency: "USD",
+    category: "electronic",
+    brand: "LG",
+    stock: 10,
+  },
+
+  {
+    name: "TV",
+    price: 1500,
+    currency: "USD",
+    category: "electronic",
+    brand: "Samsung",
+    stock: 110,
+  },
+];
+function findAboveAvg(prods) {
+  let avgPrice = 0;
+  let sumPrice = 0;
+  for (let i = 0; i < prods.lenght; i = i + 1) {
+    sumPrice = sumPrice + prods[i].price;
+  }
+}
+console.log("sumPrice:", sumPrice);
+avgPrice = sumPrice / prods.lenght;
