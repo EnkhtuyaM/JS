17b8f3db2e8fa100a89fb58268bc4f1060561ea7
EnkhtuyaM <enkhtuyam1001@gmail.com>
2023-12-11 21:51:45 +0800

    commit6

--- Problems-JS/classwork2.js
+++ Problems-JS/classwork2.js
@@ -1,10 +1,31 @@
-function isPrime(num) {
-  for (let i = 2; i < num; i = i + 1) {
-    if (num % i == 0);
-    return false;
-  }
-  return true;
-}
+// function isPrime(num) {
+//   for (let i = 2; i < num; i = i + 1) {
+//     if (num % i == 0);
+//     return true;
+//   }
+//   return false;
+// }
 
-let answer = isPrime(7);
-console.log("Answer:", answer);
+// let answer = isPrime(7);
+// console.log("Answer:", answer);
+
+function findPrime(min, max){
+  let primeNum = [];
+  let count = 0;
+  for (let i = min; i < max; i = i + 1){
+    if (min % (i+1) == 0 ){
+      primeNum[count]=i;
+      count++;
+    }
+       
+  }
+  return primeNum;
+}
+let primeNum = findPrime(1, 100);
+console.log(primeNum)
+    
+  
+  
+
+// let answer = findPrime(0, 100);
+// console.log("Answer:", answer)
