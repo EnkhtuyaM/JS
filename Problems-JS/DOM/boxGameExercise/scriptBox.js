66b19f71467b26deec3caeadc9dd57a0218a6572
EnkhtuyaM <enkhtuyam1001@gmail.com>
2023-12-22 08:38:38 +0800

    commit20

---
+++ Problems-JS/DOM/boxGameExercise/scriptBox.js
@@ -0,0 +1,31 @@
+const document = getElementById("root");
+const startButton=document.createElement('button');
+startButton.innerText="Start";
+startButton.setAttribute("class","startButton");
+startButton.addEventListener("click", startGame);
+root.appendChild(startButton);
+
+const boxes=document.createElement("div");
+boxes.setAttribute("class", "boxes");
+root.appendChild(boxes);
+
+function startGame(){
+    boxes.innerHTML="";
+    const red=Math.floor(Math.random()*215)+40;
+    const green=Math.floor(Math.random()*215)+40;
+    const blue=Math.floor(Math.random()*215)+40;
+    const randomColor=`rgb(${red},${green},${blue})`;
+    const differentColor=`rgb(${red-40},${green-40},${blue-40})`;
+    const random=Math.floor(Math.random()*8);
+    for(let i=0; i<4; i++){
+        const box=document.createElement("div");
+        box.setAttribute("class", 'box');
+        if(i==random){
+            box.style.backgroundColor=differentColor;
+            box.addEventListener("click", startGame);
+        } else {
+            box.style.backgroundColor=randomColor;
+        }
+        boxes.appendChild(box);
+    }
+}
